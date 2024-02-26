e0fe4d0fcaff35d8fc500df7683203258f95bfe6
magenteam <mustaphadoghmi@gmail.com>
2024-02-26 22:23:49 +0100

    Add files via upload

---
+++ grafbase.config.ts
@@ -0,0 +1,33 @@
+import { graph, connector, config } from '@grafbase/sdk'
+
+const g = graph.Standalone()
+
+
+
+
+const stripe = connector.OpenAPI('Stripe', {
+  schema:
+    'https://raw.githubusercontent.com/magenteam/Eshopper/master/openapi-doc.json',
+  headers: (headers) => {
+    headers.set('Authorization', { forward: 'Authorization' })
+  }
+})
+
+g.datasource(stripe)
+
+export default config({
+  graph: g,
+  cache: {
+    rules: [
+      {
+        types: ['Query'],
+        maxAge: 60
+      }
+    ]
+  },
+  auth: {
+    rules: (rules) => {
+      rules.public()
+    }
+  }
+})
