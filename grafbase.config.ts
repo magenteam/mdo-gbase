b23efdf66bca9da6c4a3a2dc6f84689785da654a
sap-commerce <mustaphadoghmi@gmail.com>
2024-04-23 11:58:06 +0100

    Update grafbase.config.ts

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -5,6 +5,7 @@ const g = graph.Standalone()
 const stripe = connector.OpenAPI('Stripe', {
   schema:
     'https://raw.githubusercontent.com/magenteam/mdo-gbase/main/demo-spa.json',
+  url: 'https://spartacus-demo.eastus.cloudapp.azure.com:8443/occ/v2',
   headers: headers => {
     headers.set('Authorization', { forward: 'Authorization' })
   },
