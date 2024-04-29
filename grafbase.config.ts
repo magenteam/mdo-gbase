e825889588d77029f622efede8f4e44374f906db
sap-commerce <mustaphadoghmi@gmail.com>
2024-04-29 11:35:35 +0100

    Update grafbase.config.ts

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -6,7 +6,7 @@ const stripe = connector.OpenAPI('Stripe', {
   schema:
     'https://raw.githubusercontent.com/magenteam/Eshopper/master/local.json',
   headers: headers => {
-    headers.set('Authorization', { forward: 'Authorization' })
+    headers.set('Authorization', `Bearer ${g.env('STRIPE_API_KEY')}`)
   },
   transforms: schema => {
     schema.queryNaming('OPERATION_ID')
