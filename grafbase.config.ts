4ab9d4c03dd2c826ca76e93b8f4aa5206efef3c4
sap-commerce <mustaphadoghmi@gmail.com>
2024-05-06 11:37:39 +0100

    Update grafbase.config.ts

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -4,9 +4,9 @@ const g = graph.Standalone()
 
 const stripe = connector.OpenAPI('Stripe', {
   schema:
-    'https://raw.githubusercontent.com/sap-accelerator-sqli/store-front/main/api-docs.json',
-  headers: headers => {
-    headers.set('Authorization', `Bearer ${g.env('STRIPE_API_KEY')}`)
+    'https://raw.githubusercontent.com/sap-accelerator-sqli/store-front/main/api-docs.json?token=GHSAT0AAAAAACRYYYJSBJGBO4VHR3OM4WXSZRYV7BA',
+  headers: (headers) => {
+    headers.set('Authorization', { forward: 'Authorization' })
   },
 })
 
